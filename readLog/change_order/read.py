8450a95b3da1e06b8efe781935514424bafa921b
SunWei <skysunwei@gmail.com>
2016-07-29 17:36:59 +0800

    add

--- readLog/change_order/read.py
+++ readLog/change_order/read.py
@@ -1,4 +1,12 @@
-for line in open('old'):
-    alls = line.split("()")
-    print alls[2].count("http")
+# coding: utf-8
 
+import csv
+
+csvfile = file('rank_2016-07-29.csv', 'rb')
+reader = csv.reader(csvfile)
+
+for line in reader:
+    print 'insert `op-sunwei-merchtype-mapping`(`merchtypeid`,`product`) values(' + \
+          str(line[0]) + ', \'' + line[3] + '\');'
+
+csvfile.close() 
