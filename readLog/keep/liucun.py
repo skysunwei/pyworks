00d88709658356f330a5bcbefa217bceca32ef70
SunWei <skysunwei@gmail.com>
2016-07-29 15:55:47 +0800

    add

--- readLog/keep/liucun.py
+++ readLog/keep/liucun.py
@@ -1,29 +1,36 @@
+# coding: utf-8
+
+import csv
+
+current_month = 7
+next_month = current_month + 1
+
 new_users = {}
-for i in range(1, 8):
+for i in range(1, next_month):
     new_users[i] = []
 
 users = {}
-for i in range(1, 8):
+for i in range(1, next_month):
     users[i] = []
 
 stay_users = {}
-for i in range(1, 8):
+for i in range(1, next_month):
     stay_users[i] = []
 
 for line in open('months_bak'):
-    datas = line.strip('\n').split(',')
+    line_data = line.strip('\n').split(',')
 
-    for i in range(1, len(datas)):
-        if int(datas[i]) > 0:
-            new_users[i].append(datas[0])
+    for i in range(1, len(line_data)):
+        if int(line_data[i]) > 0:
+            new_users[i].append(line_data[0])
             break
 
 for line in open('months_bak'):
-    datas = line.strip('\n').split(',')
+    line_data = line.strip('\n').split(',')
 
-    for i in range(1, len(datas)):
-        if int(datas[i]) > 0:
-            users[i].append(datas[0])
+    for i in range(1, len(line_data)):
+        if int(line_data[i]) > 0:
+            users[i].append(line_data[0])
             continue
 
 for i in new_users.keys():
@@ -33,11 +40,25 @@ for i in new_users.keys():
             # print stay
             stay_users[i].append(stay)
 
-headline = ''
-for i in range(1, 8):
-    headline += ',' + str(i) + 'yue'
 
-print headline
+headline = ['月份']
+for i in range(1, next_month):
+    headline.append(str(i) + '月')
+
+# print headline
+
+output_file = file('liucun.csv', 'wb')
+writer = csv.writer(output_file)
+writer.writerow(headline)
+
+data = []
 
 for i in stay_users.keys():
-    print str(i) + 'yue,' + str(stay_users[i])
+    data.append([str(i) + '月'] + ['']*(current_month - len(stay_users[i])) + stay_users[i])
+    # print str(i) + '月,' + str(stay_users[i])
+
+# print data
+
+writer.writerows(data)
+
+output_file.close()
