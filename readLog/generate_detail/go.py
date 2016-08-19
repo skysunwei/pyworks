a17f042cb09d4cd5813369e97a74409e7c513690
SunWei <skysunwei@gmail.com>
2016-08-19 19:43:32 +0800

    提交

--- readLog/generate_detail/go.py
+++ readLog/generate_detail/go.py
@@ -1,20 +1,22 @@
-folder = 'import'
+#-*- coding: UTF-8 -*-
+
+folder = 'jinguo/'
 
 tabs = open(folder + 'tabs.html').read()
 
-head = open(folder + 'head.html').read()
-foot = open(folder + 'foot.html').read()
+head = open(folder + '开头.txt').read()
+foot = open(folder + '结尾.txt').read()
 
-first = open(folder + '1.html').read()
-second = open(folder + '2.html').read()
-third = open(folder + '3.html').read()
+first = open(folder + '绿奇异果.txt').read()
+second = open(folder + '金奇异果.txt').read()
+# third = open(folder + '3.txt').read()
 
-rep = tabs.replace("diyige", first).replace("dierge", second).replace("disange", third)
+rep = tabs.replace("diyige", first).replace("dierge", second)
 
 result = head + rep + foot
 
 
-f=file(folder + "result.html","w+")
+f=file(folder + "result.txt", "w+")
 f.writelines(result)
 f.close()
 
