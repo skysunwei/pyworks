d1e63b82c46c31997243ad16f636d710584937b8
SunWei <skysunwei@gmail.com>
2016-08-31 15:12:26 +0800

    提交

--- readLog/spider/higo.py
+++ readLog/spider/higo.py
@@ -1,9 +1,9 @@
-#-*- coding: UTF-8 -*-
-
 import json
 import requests
 
-login_url = 'http://higo-express.cn/system/login.do'
+host_url = 'http://higo-express.cn/'
+login_url = host_url + 'system/login.do'
+download_url = host_url + 'wms/report/ajax/queryStockReport.do?warehouseID=1&ownerID=617'
 
 username = "C00000083"
 password = "yhdx_5ixc"
@@ -13,8 +13,6 @@ para = {
     'loginPassword': password,
 }
 
-url = 'http://higo-express.cn/wms/report/ajax/queryStockReport.do?warehouseID=1&ownerID=617'
-
 headers = {
     'Host': 'higo-express.cn',
     'Content-Length': '55',
@@ -22,18 +20,19 @@ headers = {
     'Origin': 'http://higo-express.cn',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
     'Content-Type': 'application/json',
-    'Referer': 'http://higo-express.cn/',
+    'Referer': host_url,
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,ja;q=0.2'
 }
 
-s = requests.Session()  # 可以在多次访问中保留cookie
-s.get('http://higo-express.cn/')
+s = requests.Session()
+s.get(host_url)
+s.post(login_url, data=json.dumps(para), headers=headers)
+r = s.get(download_url)
 
-result = s.post(login_url, data=json.dumps(para), headers=headers)  # POST帐号和密码，设置headers
+r.encoding='utf-8'
 
-print result
+print r.encoding
+print r.content
 
-r = s.get(url)  # 已经是登录状态了
-
-print r
+# print stores.encode(r.encoding).decode('utf-8')
