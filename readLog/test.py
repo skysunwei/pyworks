6a8edad441c809861eb0cf24034d66413f49e9c9
SunWei <skysunwei@gmail.com>
2016-11-14 10:03:31 +0800

    提交

--- readLog/test.py
+++ readLog/test.py
@@ -1,11 +1,56 @@
-higo = []
+# coding:utf-8
 
-for line in open('higo'):
-    higo.append(line.strip('\n'))
+from numpy import *
 
-for line in open('yhdx'):
-    if line.strip('\n') not in higo:
-        print line
+
+def loadData():#coding:utf-8
+
+from numpy import *
+
+def loadData():
+    return [[1,1,1,0,0],
+             [2,2,2,0,0],
+             [3,3,3,0,0],
+             [5,5,3,2,2],
+             [0,0,0,3,3],
+             [0,0,0,6,6]]
+
+data=loadData()
+
+u,sigma,vt=linalg.svd(data)
+
+print sigma
+
+sig3=mat([[sigma[0],0,0],
+      [0,sigma[1],0],
+      [0,0,sigma[2]]])
+
+print u[:,:3]*sig3*vt[:3,:]
+    return [1, 1, 1, 0, 0]
+
+
+data = loadData()
+
+u, sigma, vt = linalg.svd(data)
+
+print sigma
+
+sig3 = mat([[sigma[0], 0, 0],
+            [0, sigma[1], 0],
+            [0, 0, sigma[2]]])
+
+print u[:, :3] * sig3 * vt[:3, :]
+
+
+
+# higo = []
+#
+# for line in open('higo'):
+#     higo.append(line.strip('\n'))
+#
+# for line in open('yhdx'):
+#     if line.strip('\n') not in higo:
+#         print line
 
 # controllers = {}
 
