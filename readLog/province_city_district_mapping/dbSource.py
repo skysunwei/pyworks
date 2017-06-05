e29e400d58b41464682ee0e03291c4ba2b002bf6
SunWei <skysunwei@gmail.com>
2017-06-05 16:30:39 +0800

    no message

--- readLog/province_city_district_mapping/dbSource.py
+++ readLog/province_city_district_mapping/dbSource.py
@@ -13,8 +13,10 @@ cnn = umysql.Connection()
 def mysql_connect():
     cnn.connect(DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DB)
 
+
 def mysql_query(sql):
     return cnn.query(sql)
+
 
 def mysql_close():
     cnn.close()
