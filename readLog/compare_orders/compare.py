c89cc84947d3d87611d8c7fa0430894b0592af67
SunWei <skysunwei@gmail.com>
2017-08-04 10:17:56 +0800

    no message

--- readLog/compare_orders/compare.py
+++ readLog/compare_orders/compare.py
@@ -18,29 +18,30 @@
 #         print line
 
 
-weixin = []
+zhongxin = []
 yhdx = []
 
-folder = '17-6'
+folder = '17-7'
 
-for line in open(folder + '/b'):
-    weixin.append(line.strip('\n'))
+for line in open(folder + '/zhongxin'):
+    zhongxin.append(line.strip('\n'))
 
-for line in open(folder + '/a'):
+for line in open(folder + '/yhdx'):
     yhdx.append(line.strip('\n').split(',')[0])
 
-print len(weixin)
+print len(zhongxin)
 print len(yhdx)
 
 a = {}
-for i in yhdx:
+for i in zhongxin:
     a[i] = yhdx.count(i)
 
 # print a
 
 b = {}
-for i in weixin:
-    b[i] = weixin.count(i)
+
+for i in zhongxin:
+    b[i] = zhongxin.count(i)
 
 c = {}
 
@@ -52,21 +53,22 @@ for i in c:
     print i, a[i], b[i]
 
 
-print [i for i in weixin if i not in yhdx]
+print 'zhongxin duo'
+print [i for i in zhongxin if i not in yhdx]
 
 print
+print 'yhdx duo'
+print [i for i in yhdx if i not in zhongxin]
 
-print [i for i in yhdx if i not in weixin]
 
-
-import csv
-
-csvfile = file('zhongxinmingxi.csv', 'rb')
-reader = csv.reader(csvfile)
-
-for i in reader:
-    print i
-    break
+# import csv
+#
+# csvfile = file('zhongxinmingxi.csv', 'rb')
+# reader = csv.reader(csvfile)
+#
+# for i in reader:
+#     print i
+#     break
 
 
 # print
