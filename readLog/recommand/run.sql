4361770bc5168e6c35f1187586de99cfabde5d9b
SunWei <skysunwei@gmail.com>
2018-03-13 19:23:30 +0800

    no message

--- readLog/recommand/run.sql
+++ readLog/recommand/run.sql
@@ -1,29 +1,22 @@
 # 线上查询库：https://msn.youhaodongxi.com/index.php   User name: readonly   Password: Readonly2016
 # https://cp.youhaodongxi.com/admin/sales/setchannel
 
-SELECT DISTINCT saler.userid, saler.pid, a.tel ,
-(CASE
-  WHEN a.buyerid > 0 THEN 1
-                                             ELSE 0
-                                         END) 'vip'
-FROM saler
+-- 购买人和推荐人关系表
+
+SELECT a.user_id,
+       a.recommend_user_id,
+       b.tel,
+       '1' AS '1'
+FROM
+  (SELECT vipuser_rebate.order_id,
+          vipuser_rebate.user_id,
+          vipuser_rebate.recommend_user_id
+   FROM vipuser_rebate
+   WHERE vipuser_rebate.order_status=2) a
 LEFT JOIN
-(
-	SELECT
-    	payorder.buyerid,
-    	recipientaddress.tel
-    FROM payorder, paysuborder, recipientaddress
-    WHERE
-    	payorder.addressid = recipientaddress.addressid
-    	AND payorder.orderid = paysuborder.orderid
-    	AND paysuborder.merchandiseid = 998
-) a ON a.buyerid=saler.userid
-WHERE saler.status=1
-  AND saler.pid != 0
-  AND saler.userid NOT IN (27997,
-                           38617,
-                           67413,
-                           145007,
-                           48416,
-                           89159,
-                           237)
+  (SELECT payorder.orderid,
+          recipientaddress.userid,
+          recipientaddress.tel
+   FROM payorder,
+        recipientaddress
+   WHERE payorder.addressid=recipientaddress.addressid) b ON a.order_id=b.orderid
