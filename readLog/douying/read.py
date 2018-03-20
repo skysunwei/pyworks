35d4fe3ee1d11da3b1aa9c6a146a5bc90552b8da
SunWei <skysunwei@gmail.com>
2018-03-20 17:34:31 +0800

    no message

--- readLog/douying/read.py
+++ readLog/douying/read.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python
+
 #coding:utf-8
 
 import requests
+import json
+import sys
 
-url = ''
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
-requests.get('https://github.com/timeline.json')
+# url = ''
+#
+# page = requests.get('https://www.douyin.com/aweme/v1/hot_aweme/?cursor=0&count=36&aweme_id=6532791544951344392')
+#
+# if page.status_code == 200:
+#
+#     # beijing_districts = json.loads(page.text)
+#
+#     print page.text
+
+weixin_keys = []
+
+for line in open('weixin', 'r'):
+    weixin_keys.append(line.strip('\n'))
+
+print weixin_keys
+
+f = open('temp', 'r')
+info = f.read()
+f.close()
+
+data = json.loads(info)
+
+for show in data['members']:
+    print show['sNickName'], show['sEmail'],
+
+# videos = data['aweme_list']
+# for video in videos:
+    # signature = video['author']['signature']
+    #
+    # for key in weixin_keys:
+    #     if key in signature:
+    #         print key, ',,,,', signature
