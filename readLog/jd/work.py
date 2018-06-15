d9480652d44d405f899b1db3464095a8631291e1
SunWei <skysunwei@gmail.com>
2018-06-15 17:29:23 +0800

    no message

---
+++ readLog/jd/work.py
@@ -0,0 +1,33 @@
+infos = []
+womens = []
+
+#----------
+
+for line in open('data'):
+    datas = line.strip('\n').split('\t')
+    infos.append(datas[1])
+
+infos = list(set(infos))
+
+print len(infos)
+
+#----------
+
+for line in open('women'):
+    datas = line.strip('\n').split('|')
+
+    try:
+        womens.append(datas[2])
+    except:
+        continue
+
+womens = list(set(womens))
+
+print len(womens)
+
+#----------
+
+inster = list(set(womens).intersection(set(infos)))
+
+print len(inster)
+print inster
--- readLog/json/work.py
+++ readLog/json/work.py
@@ -1,12 +1,8 @@
-__author__ = 'colin'
-
 import json
 
 outs = []
 
 for line in open('data'):
-    # print line
-
     obs = json.loads(line)
     for ob in obs:
         outs.append(ob["goods"]["name"])
